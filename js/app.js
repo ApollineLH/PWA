e6e4351ec4c2a70c1716540c418154ad4fedb722
ApollineLH <apolline.le@gmail.com>
2023-06-14 16:20:23 +0200

    feat: readme et commentaire

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
+//@Apolline LEHIRITTE
+
+/* This code is checking if the browser supports service workers by checking if the "serviceWorker"
+property exists in the "navigator" object. If it does, it registers a service worker located at
+"/service-worker.js" and logs a message to the console indicating whether the registration was
+successful or not. */
 if ("serviceWorker" in navigator) {
     navigator.serviceWorker.register("/service-worker.js")
         .then(function (registration) {
--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,17 @@
-// Nom du cache
+//@Apolline LEHIRITTE
+
 var cacheName = "offline-cache";
 
-// Liste des ressources à mettre en cache
 var resourcesToCache = [
     "offline.html",
     "css/style.css",
-    // Ajoutez d'autres fichiers à mettre en cache ici si nécessaire
 ];
 
-// Événement d'installation du service worker
+/* This code is registering an event listener for the "install" event on the service worker. When the
+service worker is installed, it will open a cache with the name "offline-cache" and add the
+resources listed in the "resourcesToCache" array to the cache. The "waitUntil" method ensures that
+the installation process is not considered complete until all the resources have been added to the
+cache. */
 self.addEventListener("install", function (event) {
     event.waitUntil(
         caches.open(cacheName).then(function (cache) {
@@ -17,7 +20,13 @@ self.addEventListener("install", function (event) {
     );
 });
 
-// Événement fetch du service worker
+/* This code is registering an event listener for the "fetch" event on the service worker. When a
+network request is made, the service worker will intercept it and try to fetch the requested
+resource from the network. If the network request fails (e.g. due to a lack of internet connection),
+the service worker will try to retrieve the resource from the cache using the `caches.match()`
+method. If the resource is found in the cache, it will be returned to the browser. If the resource
+is not found in the cache, the service worker will return the "offline.html" page, which is one of
+the resources that was previously cached during the installation process. */
 self.addEventListener("fetch", function (event) {
     event.respondWith(
         fetch(event.request).catch(function () {
